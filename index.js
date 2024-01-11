81e3d59709c3a8639e6110c5f8cd623e48bdb0b4
Dennis Audu <dennisaudu@Nexa-Macbook-Air.local>
2024-01-10 22:46:33 -0500

    vercel json added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv"
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -10,7 +12,8 @@ app.use(express.json());
 import { homeRoute } from "./routes/homePage.js";
 import { eventRoute } from "./routes/eventPage.js";
 import {contactRoute} from "./routes/contactPage.js";
-const PORT = 9800;
+
+const PORT = process.env.PORT || 6060;
 
 app.use( '/api/home', homeRoute);
 app.use( '/api/event', eventRoute);
